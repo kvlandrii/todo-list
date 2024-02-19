a44f72604ff2711cc601a1faf63060e330511fc7
kvlandrii <andrijkoval30@gmail.com>
2024-02-19 22:00:27 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />}></Route>
+            <Route path="home" element={<Home />}></Route>
             <Route path="*" element={<Error />}></Route>
 
             <Route path="about" element={<About />}></Route>
